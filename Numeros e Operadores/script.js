76f9f1cc7b33dd4acf9b2fd79982e1535b4e7d51
saintravi <Dudah4@gmail.com>
2021-08-06 06:16:00 -0300

    operadores

---
+++ Numeros e Operadores/script.js
@@ -0,0 +1,28 @@
+var operador = 100;
+var operando = 50;
+
+var resultado = operador * operando;
+
+console.log(resultado);
+
+//
+
+var nome = 'Eduarda';
+var sobrenome = 'Alves';
+
+var nomeCompleto = nome + sobrenome;
+
+console.log(nomeCompleto);
+
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nomeCompleto);
+
+//
+
+var primeiroValor = 10;
+var segundoValor = 'Azul';
+
+var valorTotal = primeiroValor - segundoValor;
+
+console.log(valorTotal);

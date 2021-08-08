fee0dc7fff94b2c2e22ef7462b9480315052b7d9
saintravi <Dudah4@gmail.com>
2021-08-08 13:57:23 -0300

    estrutura for

---
+++ Estruturas de Repetição/script.js
@@ -0,0 +1,20 @@
+var numeroSorteado = 10;
+
+for (var i = 0; i < 10; i++) {
+    console.log('Eduarda');
+}
+
+for (var i = 0; i < 100; i++) {
+    if(numeroSorteado === i) {
+        console.log('Seu numero sorteado foi encontrado');
+        break;
+    }
+}
+
+//
+
+var tabuada = 7;
+
+for(var x = 0; x <= 10; x++) {
+    console.log('Valor de ' + tabuada + ' x ' + x + ' = ' + tabuada * x);
+}

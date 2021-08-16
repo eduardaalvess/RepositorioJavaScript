b276c21de1abb81cc329929cf292798aca54a016
saintravi <Dudah4@gmail.com>
2021-08-16 05:19:18 -0300

    operador ternario e arrow, template string

---
+++ Operador Ternario e Arrow Functions/script.js
@@ -0,0 +1,17 @@
+let isValid = true;
+
+function verify(isValid) {
+    if(isValid) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const result = isValid ? true : false;
+
+let zero = 0;
+
+const numeroResultado = zero == 0 ? 0 : -1;
+
+console.log(numeroResultado);
---
+++ Template String/script.js
@@ -0,0 +1,16 @@
+let nome = 'Eduarda';
+let sobrenome = 'Alves';
+let cidade = 'Recife';
+
+
+
+// Sem template String!
+console.log(
+    'Olá, meu nome é ' +
+    nome + ' ' + sobrenome +
+    ' e eu moro em ' + cidade);
+
+
+console.log(`Olá, meu nome é ${nome} ${sobrenome} e eu moro em ${cidade}`);
+
+console.log(`A soma de 54 + 14 é: ${54 + 14}`);
